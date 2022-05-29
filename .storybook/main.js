3879a59649d5b1e5c64ea307e20da642c7057b95
caesuric <illusorycaesura@gmail.com>
2022-05-29 05:55:08 -0400

    initially added needed libraries

---
+++ .storybook/main.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // "webpackFinal": async (config, { configType }) => {
+  //   config.resolve.modules.push(path.resolve(__dirname, '../src'));
+  //   return config;
+  // },
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/preset-create-react-app"
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-webpack5"
+  }
+}
