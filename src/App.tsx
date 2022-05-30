c14cfa426caba51e43420a0cf5788692ed1403e4
caesuric <illusorycaesura@gmail.com>
2022-05-30 04:29:47 -0400

    removed storybook due to configuration issues with other dependencies, added button for scoped CSS exploration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from './Button/Button';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <Button />
+                <button className="my-button">DEF</button>
+            </div>
+        );
+    }
 }
---
+++ src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Button.scss';
+
+export default class Button extends React.Component {
+    render() {
+        return (
+            <button className="my-button">ABC</button>
+        );
+    }
+}
