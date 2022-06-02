985ce57d7f24423f1f51835f585e3537c7508569
caesuric <illusorycaesura@gmail.com>
2022-06-02 10:29:46 -0400

    added logo to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react';
 import { NavLink } from 'react-router-dom';
 import styles from './Sidebar.module.scss';
+import logo from 'assets/logo.svg';
 
 interface SidebarProps {}
 
 const Sidebar: FC<SidebarProps> = () => (
   <div className={styles.Sidebar}>
+    <img src={logo} />
     <nav>
       <ul>
         <NavLink to="/" className={({isActive}) => (isActive ? styles.activeLink : '')}>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from 'assets/logo.svg';
 import styles from './home.module.scss';
 import Button from 'components/Button/Button';
 
