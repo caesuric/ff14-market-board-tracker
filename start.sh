0c5c5b2bcd3f0b7b75ba03c1d5c65012ad1696d0
caesuric <illusorycaesura@gmail.com>
2024-07-10 08:29:40 -0400

    removed -s command from start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-serve -l 443 -s --ssl-cert /etc/letsencrypt/live/xivmarketstats.com/fullchain.pem --ssl-key /etc/letsencrypt/live/xivmarketstats.com/privkey.pem build
+serve -l 443 --ssl-cert /etc/letsencrypt/live/xivmarketstats.com/fullchain.pem --ssl-key /etc/letsencrypt/live/xivmarketstats.com/privkey.pem build
