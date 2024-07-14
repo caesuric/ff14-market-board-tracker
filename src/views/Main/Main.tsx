60ec952e7f236345de36a1b36007e1c7c197cf80
caesuric <illusorycaesura@gmail.com>
2024-07-13 20:48:21 -0400

    updated site to have nicer navigation tabs

--- src/views/Main/Main.tsx
+++ src/views/Main/Main.tsx
@@ -1,30 +1,32 @@
-import React, { FC } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import React, { FC, SyntheticEvent, useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import styles from "./Main.module.scss";
+import { Tab, Tabs } from "@mui/material";
 
 interface MainProps {}
 
 const Main: FC<MainProps> = () => {
+  const [value, setValue] = useState("itemTracker");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleChange = (event: SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+    if (newValue === "trackByJob") navigate("/track-by-job");
+    else navigate("/");
+  };
+  useEffect(() => {
+    if (location.pathname === "/track-by-job") setValue("trackByJob");
+    else setValue("itemTracker");
+  }, [location.pathname]);
+
   return (
     <div className={styles.Main}>
       <div className={styles.topBar}>
         <img className={styles.logo} src="/logo192.png" alt="Site logo." />
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? styles.activeLink : styles.inactiveLink
-          }
-        >
-          <div className={styles.topButton}>Item Tracker</div>
-        </NavLink>
-        <NavLink
-          to="/track-by-job"
-          className={({ isActive }) =>
-            isActive ? styles.activeLink : styles.inactiveLink
-          }
-        >
-          <div className={styles.topButton}>Track By Job</div>
-        </NavLink>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Item Tracker" value="itemTracker" />
+          <Tab label="Track By Job" value="trackByJob" />
+        </Tabs>
       </div>
       <div className={styles.mainPanel}>
         <Outlet />
