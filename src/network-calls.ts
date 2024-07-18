b614fa8662b2f376deebbf097619ce18bcd02dc2
caesuric <illusorycaesura@gmail.com>
2024-07-18 09:48:44 -0400

    fixed issue with UUID pulling from job start in network calls

--- src/network-calls.ts
+++ src/network-calls.ts
@@ -1,57 +1,68 @@
 export const getWorlds = async () => {
-    const worldDataResponse = await fetch(
-        "https://xivmarketstats.com:1414/rest/worlds"
-      );
-      const worldData = await worldDataResponse.json();
-      return worldData;
+  const worldDataResponse = await fetch(
+    "https://xivmarketstats.com:1414/rest/worlds"
+  );
+  const worldData = await worldDataResponse.json();
+  return worldData;
 };
 
 export const getTaxRates = async (world: string) => {
-    const taxRatesResponse = await fetch(
-        `https://xivmarketstats.com:1414/rest/tax-rates/${world}`
-    );
-    const taxRatesData = await taxRatesResponse.json();
-    return taxRatesData;
-}
+  const taxRatesResponse = await fetch(
+    `https://xivmarketstats.com:1414/rest/tax-rates/${world}`
+  );
+  const taxRatesData = await taxRatesResponse.json();
+  return taxRatesData;
+};
 
 export const startGetCurrentMarketData = async (world: string, ids: any[]) => {
-    const idsCommaSeparated = ids.join(",");
-      const currentResponse = await fetch(
-        `https://xivmarketstats.com:1414/rest/market-current/start/${world}/${idsCommaSeparated}`
-      );
-      const currentResponseData = await currentResponse.json();
-      return currentResponseData;
-}
+  const idsCommaSeparated = ids.join(",");
+  const currentResponse = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-current/start/${world}/${idsCommaSeparated}`
+  );
+  const currentResponseData = await currentResponse.text();
+  return currentResponseData;
+};
 
-export const startGetHistoricalMarketData = async (world: string, ids: any[]) => {
-    const idsCommaSeparated = ids.join(",");
-    const historicalResponse = await fetch(
-        `https://xivmarketstats.com:1414/rest/market-current/start/${world}/${idsCommaSeparated}`
-      );
-      const historicalResponseData = await historicalResponse.json();
-      return historicalResponseData;
-}
+export const startGetHistoricalMarketData = async (
+  world: string,
+  ids: any[]
+) => {
+  const idsCommaSeparated = ids.join(",");
+  const historicalResponse = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-current/start/${world}/${idsCommaSeparated}`
+  );
+  const historicalResponseData = await historicalResponse.text();
+  return historicalResponseData;
+};
 
 export const checkCurrentMarketDataJob = async (jobUuid: string) => {
-    const response = await fetch(`https://xivmarketstats.com:1414/rest/market-current/status/${jobUuid}`);
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-current/status/${jobUuid}`
+  );
+  const responseData = await response.json();
+  return responseData;
 };
 
 export const checkHistoricalMarketDataJob = async (jobUuid: string) => {
-    const response = await fetch(`https://xivmarketstats.com:1414/rest/market-historical/status/${jobUuid}`);
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-historical/status/${jobUuid}`
+  );
+  const responseData = await response.json();
+  return responseData;
 };
 
 export const getCurrentMarketData = async (jobUuid: string) => {
-    const response = await fetch(`https://xivmarketstats.com:1414/rest/market-current/results/${jobUuid}`);
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-current/results/${jobUuid}`
+  );
+  const responseData = await response.json();
+  return responseData;
 };
 
 export const getHistoricalMarketData = async (jobUuid: string) => {
-    const response = await fetch(`https://xivmarketstats.com:1414/rest/market-historical/results/${jobUuid}`);
-    const responseData = await response.json();
-    return responseData;
+  const response = await fetch(
+    `https://xivmarketstats.com:1414/rest/market-historical/results/${jobUuid}`
+  );
+  const responseData = await response.json();
+  return responseData;
 };
